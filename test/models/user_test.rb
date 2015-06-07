7d17142f0733bdbf3a6d88432ef5006e536a90be
drej2k <andrevidic@hotmail.com>
2015-06-07 18:17:32 +1000

    alter duplicate email test to also cover case insensitivity

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,4 +50,11 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test "email addresses should be unique even with case changes" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
