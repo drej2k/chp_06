35277d1c8f6d8400ca7dc243a63d0ac143c018d8
drej2k <andrevidic@hotmail.com>
2015-06-07 19:19:36 +1000

    add password validation to User model and update test password to >8 characters

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-  	@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+  	@user = User.new(name: "Example User", email: "user@example.com", password: "foobarbaz", password_confirmation: "foobarbaz")
   end
 
   test "should be valid" do
